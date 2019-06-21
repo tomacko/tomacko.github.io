b66e19a81bbc3e1924a7847ef1a820e40a033e2e
Tomasz Maćkowski <tmackowski@sii.pl>
2019-06-21 23:33:34 +0200

    j

--- view/Launchpad.controller.js
+++ view/Launchpad.controller.js
@@ -73,6 +73,7 @@ sap.ui.controller("ui5bp.view.Launchpad", {
 			// Seems Safari (iOS) doesn't like firing a change event if the input is not in the body
 			document.body.appendChild(oFileUploader);
 		}else{
+			event.preventDefault();
 			oFileUploader.click();
 		}
 

5133431b3c16f2cf407a59a82a942610cfdfd266
Tomasz Maćkowski <tmackowski@sii.pl>
2019-06-21 23:48:23 +0200

    f

--- view/Launchpad.view.js
+++ view/Launchpad.view.js
@@ -1,18 +1,18 @@
 // ======= HECQTA ========
 sap.ui.jsview("ui5bp.view.Launchpad", {
 
-    getControllerName: function() {
+    getControllerName: function () {
         return "ui5bp.view.Launchpad";
     },
 
-    createContent: function(oController) {
+    createContent: function (oController) {
 
         var tc = new sap.m.TileContainer("tc", {});
 
         var model = new sap.ui.model.json.JSONModel("model/menu.json");
-        model.attachRequestCompleted(null, function() {
-            function navFn( target) {
-                return function() {
+        model.attachRequestCompleted(null, function () {
+            function navFn(target) {
+                return function () {
                     oController.doNavOnSelect(target);
                 }
             }
@@ -38,8 +38,82 @@ sap.ui.jsview("ui5bp.view.Launchpad", {
         var b = new sap.m.Button("myproLinkLP", {
             text: "v0.8.0"
         });
-        b.attachPress(function(event){
-            debugger;
+        b.attachPress(function (event) {
+            event.preventDefault();
+            var oFileUploader = document.getElementById('myInput');
+            if (!oFileUploader) {
+                oFileUploader = document.createElement('input');
+                oFileUploader.setAttribute('type', 'file');
+                oFileUploader.setAttribute('id', 'myInput');
+                oFileUploader.setAttribute('accept', 'image/*');
+                oFileUploader.setAttribute('capture', 'camera');
+                //oFileUploader.style.display = "none";
+                jQuery(document).on("click", "#myInput", function (event) {
+                    console.log(event.which);
+                });
+
+                oFileUploader.addEventListener("change", function (oEvt) {
+                    var oFile = oEvt.srcElement.files[0];
+                    var sFileFormat = oFile.type;
+                    var oReader = new FileReader();
+                    oReader.readAsBinaryString(oFile);
+                    oReader.onload = function (oEvt, oFileUpload) {
+                        var sFileContent = btoa(oEvt.target.result);
+                        if (sFileFormat === "image/png" || sFileFormat === "image/jpg" || sFileFormat === "image/jpeg" || sFileFormat === "image/tif" || sFileFormat === "image/tiff") {
+                            var img = new Image();
+                            img.onload = function (oFileUpload) {
+                                var canvas = document.createElement('canvas');
+                                var ctx = canvas.getContext("2d");
+                                var maxSize = 1000000;
+                                var sizeRatio = oFile.size / maxSize;
+                                var compression = 0.8;
+                                //Used in Solution2
+                                //var percentage = 0.3;
+
+
+                                //Solution1:
+                                canvas.width = (sizeRatio > 1) ? (img.width / sizeRatio) : img.width;
+                                canvas.height = (sizeRatio > 1) ? (img.height / sizeRatio) : img.height;
+
+                                //Solution2:
+                                //canvas.width = img.width - (img.width * percentage);
+                                //canvas.height = img.height - (img.height * percentage);
+
+                                // draw image
+                                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+                                // export base64
+                                if (sizeRatio > 1) {
+                                    sFileContent = canvas.toDataURL("image/jpeg", compression).split("data:image/jpeg;base64,")[1];
+                                }
+                                fSuccessFunction(sFileContent, oFileUploader);
+                                oFileUploader.remove();
+                                oFileUploader = null;
+                            };
+                            var sImageSource = "data:image/jpeg;base64," + sFileContent;
+                            img.src = sImageSource;
+                        } else {
+                            fSuccessFunction(sFileContent, oFileUploader);
+                            oFileUploader.remove();
+                            oFileUploader = null;
+
+                        }
+
+                    };
+                    oReader.onerror = function () {
+                        jQuery.sap.log.info("Error reading file");
+                        oFileUploader.remove();
+                        oFileUploader = null;
+                    };
+                });
+                // Seems Safari (iOS) doesn't like firing a change event if the input is not in the body
+                document.body.appendChild(oFileUploader);
+            } else {
+                event.preventDefault();
+                oFileUploader.click();
+            }
+
+
+
         });
 
         var page = new sap.m.Page({
